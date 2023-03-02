06d7c445b3ed568d1387e4888d1f61c1bdc0aad4
AgneRacyte250 <1racya54@solent.ac.uk>
2023-03-02 21:34:46 +0000

    output

---
+++ calculater.py
@@ -0,0 +1,8 @@
+print(" Provide 2 numbers: ")
+n1=float(input())
+n2=float(input())
+print(f"{n1} + {n2} = {n1 + n2}")
+print("{} - {} = {} ". format(n1, n2,n1-n2)) # method mode .Format
+print(str(n1), + " x " + str(n2) + " = " + str(n1*n2))  #contantition method (combining strings )
+print(str(n1), + " / " + str(n2), + " = " + str(n1/n2))  #string combiantion by ,
+print(f"{n1} ^ {n2} = {n1**n2}")
---
+++ week 1/output.py
@@ -0,0 +1,3 @@
+name = input("what is your name?  ")
+age = input ("How old are you?  ")
+print (f"Hello {name} you are {age} years old ")
