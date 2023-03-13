80b5f7eea8b1963f6fc642111ac9144991921023
AgneRacyte250 <1racya54@solent.ac.uk>
2023-03-13 10:56:16 +0000

    changed folders

--- Week 3/Activities/function_calls.py
+++ Week 3/Activities/function_calls.py
@@ -11,6 +11,34 @@ def upper(word):
 
 # slicing - and act of accsesing of individual items of a list . works on a charecter of a string . slosing using
 # [start:end:steps ] spesifing
+#word="This is sparta"
+#print(word[3:8:1])
+def mirro(word):
+    print(word,"|", word[::-1])
 
-word="This is sparta"
-print(word[3:8:1])
+def repeat(word):
+    number=int(input("How many times to repeat: "))
+    for i in range(number):
+        if i % 2 == 0:
+            word.upper()
+        else:
+            word.lower()
+def run():
+    w= input("Enter a word: ")
+    print("Choose an opptoin:\n1.Box\n2.Lower case\n3.upper case\n4.Mirror\n5.Repeat")
+    opt= int(input())
+    if opt==1:
+        box(w)
+    elif opt==2:
+        low(w)
+    elif opt == 3:
+        upper(w)
+    elif opt ==4:
+         mirro(w)
+    elif opt ==5:
+        repeat(w)
+    else:
+        print("No such option!!!")
+
+
+run()
